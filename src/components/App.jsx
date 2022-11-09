1b8ab6e71da1402be0172162a758fb26aa784dd9
Anna Karachkovska <akarachkovska@gmail.com>
2022-11-09 19:37:08 +0200

    Add lazy

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,24 @@
+import { lazy, Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
+
 import Navigator from "./Navigator/Navigator";
-import Home from "./Home/Home";
-import Movies from "./Movies/Movies";
-import MovieDetails from "components/MovieDetails/MovieDetails";
-import Cast from "./Cast/Cast";
-import Reviews from "./Reviews/Reviews";
+
+// import Home from "./Home/Home";
+// import Movies from "./Movies/Movies";
+// import MovieDetails from "components/MovieDetails/MovieDetails";
+// import Cast from "./Cast/Cast";
+// import Reviews from "./Reviews/Reviews";
+const Home = lazy(() => import("./Home/Home"));
+const Movies = lazy(() => import("./Movies/Movies"));
+const MovieDetails = lazy(() => import("components/MovieDetails/MovieDetails"));
+const Cast = lazy(() => import("./Cast/Cast"));
+const Reviews = lazy(() => import("./Reviews/Reviews"));
 
 const App = () => {
   return (
     <div>
       <Navigator/>
+      <Suspense fallback={<div>Loading...</div>}>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/movies" element={<Movies />}/>
@@ -19,6 +28,7 @@ const App = () => {
         </Route>
         <Route path="*" element={<Home />} />
       </Routes>
+      </Suspense>
     </div>
   );
 };
